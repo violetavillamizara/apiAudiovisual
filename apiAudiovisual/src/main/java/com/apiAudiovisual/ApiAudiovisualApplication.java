b6bd92e57bfa8a1e44a0272f8891029c3201d9cb
violetavillamizara <villamizaravioleta@gmail.com>
2024-04-01 07:24:50 -0500

    feat: :tada: Beginning project
    new project with securtity and dependencies

---
+++ apiAudiovisual/src/main/java/com/apiAudiovisual/ApiAudiovisualApplication.java
@@ -0,0 +1,13 @@
+package com.apiAudiovisual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAudiovisualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAudiovisualApplication.class, args);
+	}
+
+}
---
+++ apiAudiovisual/src/main/java/com/apiAudiovisual/domain/security/WebSecurityConfig.java
@@ -0,0 +1,49 @@
+package com.apiAudiovisual.domain.security;
+
+import com.apiAudiovisual.domain.security.Constans;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig {
+    @Autowired
+    JWTAuthorizationFilter jwtAuthorizationFilter;
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers(HttpMethod.POST, Constans.LOGIN_URL, Constans.REGISTER_URL).permitAll()
+                        // Allow Swagger documentation HTML
+                        .requestMatchers(HttpMethod.GET,
+                                "/doc/**",
+                                "/swagger-ui.html",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/api/v1/auth/**",
+                                "/v2/api-docs",
+                                "/v3/api-docs/**",
+                                "/swagger-resources",
+                                "/swagger-resources/**",
+                                "/configuration/ui",
+                                "/configuration/security",
+                                "/swagger-ui/**",
+                                "/webjars/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
+                        .anyRequest().authenticated())
+                .addFilterAfter(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
